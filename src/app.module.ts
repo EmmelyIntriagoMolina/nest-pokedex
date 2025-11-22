4afd93ef13496c67a95043247c68074bb259cc87
Emmely Gabriela <gabyintriago07@gmail.com>
2025-11-22 12:24:18 -0500

    Configuration loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ]
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public')
     }),
